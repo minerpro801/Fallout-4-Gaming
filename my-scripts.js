21a6c00a725dde499a0b9862369ac8b04bf5791e
minerpro801 <40616681+minerpro801@users.noreply.github.com>
2018-06-26 21:04:33 -0600

    Add files via upload

---
+++ my-scripts.js
@@ -0,0 +1,34 @@
+if(localStorage.getItem("count") == null) {
+	localStorage.setItem("count" , 0);
+}
+
+function update(){
+	document.getElementById("count").innerHTML = localStorage.getItem("count");
+}
+
+update();
+
+function plusOne(){
+	localStorage.setItem("count" , Number(localStorage.getItem("count")) + 1);
+	update();
+}
+
+//reset count
+function reset() {
+	if (confirm("Are you sure you want to reset?")) {
+    	localStorage.setItem("count",0);
+		update();
+	}
+}
+
+// save-to-phone instructions
+var instructions = document.getElementById("instructions");
+instructions.style.display = "none";
+
+function toggleInstructions() {
+	if (instructions.style.display === "none") {
+		instructions.style.display = "block";
+	} else {
+		instructions.style.display = "none";
+	}
+}
